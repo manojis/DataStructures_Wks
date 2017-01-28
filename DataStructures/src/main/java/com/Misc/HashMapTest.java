bd3109ab4374d9a08da3f149d24c20144c0888e9
manoj_m <manojis@live.com>
2017-01-28 22:59:33 +0530

    updated

---
+++ DataStructures/src/main/java/com/Misc/HashMapTest.java
@@ -0,0 +1,37 @@
+package com.Misc;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by 212561694 on 1/24/2017.
+ */
+public class HashMapTest {
+
+    /**
+     * @author Arpit Mandliya
+     */
+    public static void main(String[] args) {
+        // create hash map
+        HashMap<Integer,String> newmap = new HashMap();
+
+        // populate hash map
+        newmap.put(1, "tutorials");
+        newmap.put(2, "point");
+        newmap.put(3, "is best");
+
+        /*for(HashMap.Entry<Integer, String> entry : newmap.entrySet()) {
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+        }*/
+
+        Iterator<Integer> iter = newmap.keySet().iterator();
+
+        while(iter.hasNext()){
+            Integer keyVal = iter.next();
+            String value = newmap.get(keyVal);
+            System.out.println("Key set values are: " + value);
+        }
+    }
+}
