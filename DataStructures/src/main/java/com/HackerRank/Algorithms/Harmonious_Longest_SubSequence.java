3f44593ac01276a032d6344dd4228f50e23dadaf
manojis <manojm_nair@outlook.com>
2017-08-14 15:21:31 -0500

    kill process

---
+++ DataStructures/src/main/java/com/HackerRank/Algorithms/Harmonious_Longest_SubSequence.java
@@ -0,0 +1,20 @@
+package com.HackerRank.Algorithms;
+
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+
+import java.util.HashMap;
+
+/**
+ * @author Manoj.Mohanan Nair
+ * @Date 8/13/17
+ */
+public class Harmonious_Longest_SubSequence {
+
+    public static void main(String[] args){
+
+       System.out.println(System.getProperty("user.home"));
+
+
+
+    }
+}
---
+++ DataStructures/src/main/java/com/HackerRank/Algorithms/Kill_Process.java
@@ -0,0 +1,35 @@
+package com.HackerRank.Algorithms;
+
+import java.util.*;
+
+/**
+ * @author Manoj.Mohanan Nair
+ * @Date 8/14/17
+ */
+public class Kill_Process {
+
+    public static void main(String[] args){
+
+    }
+
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < ppid.size(); i++){
+            map.computeIfAbsent(ppid.get(i), k -> new ArrayList<Integer>()).add(pid.get(i));
+        }
+        List<Integer> ans = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(kill);
+        while (!queue.isEmpty()){
+            int target = queue.poll();
+            ans.add(target);
+            if (map.containsKey(target)){
+                List<Integer> tmp = map.get(target);
+                for (int num : tmp){
+                    queue.offer(num);
+                }
+            }
+        }
+        return ans;
+    }
+}
