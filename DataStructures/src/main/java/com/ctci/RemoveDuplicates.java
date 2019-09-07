8c582b1643ded096e87c6b7b18536b41c229698d
manoj <manojm_nair@outlook.com>
2019-09-06 23:59:55 -0700

    change list 23-59

---
+++ DataStructures/src/main/java/com/ctci/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+package com.ctci;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+/**
+ * @author Manoj.Mohanan Nair
+ * @Date 9/6/19
+ * Remove Duplicates from a list
+ */
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        LinkedList<String> uList = new LinkedList<String>();
+        uList.add("Manoj");
+        uList.add("Mohan");
+        uList.add("Lalitha");
+        uList.add("arya");
+        uList.add("manvi");
+        uList.add("Manoj");
+        uList.add("manvi");
+
+        Set<String> dSet = new HashSet<String>(uList);
+        dSet.forEach(element -> System.out.println(element));
+    }
+}
---
+++ DataStructures/src/main/java/com/stackAndQueue/CircularQueueLinkedList.java
@@ -0,0 +1,9 @@
+package com.stackAndQueue;
+
+/**
+ * @author Manoj.Mohanan Nair
+ * @Date 9/6/19
+ * Create a circularQueue using LinkedList
+ */
+public class CircularQueueLinkedList {
+}
