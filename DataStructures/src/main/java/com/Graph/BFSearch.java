dc1eb8e8c60a4857a89e9b9db4b89be9793b4c78
manoj <manojm_nair@outlook.com>
2019-09-09 13:58:55 -0700

    Adding the recursion

--- DataStructures/src/main/java/com/Graph/BFSearch.java
+++ DataStructures/src/main/java/com/Graph/BFSearch.java
@@ -4,5 +4,17 @@ package com.Graph;
  * @author Manoj.Mohanan Nair
  * @Date 8/15/17
  */
+
+//finding the shortest path between root node A and target node G
+/**           A
+       /      |     \
+     D        C     B
+      \       /  \  |
+       \     F      E
+        \   /
+          G
+ **/
 public class BFSearch {
+
+
 }
---
+++ DataStructures/src/main/java/com/dynamicrecursion/ReverseString.java
@@ -0,0 +1,30 @@
+package com.dynamicrecursion;
+
+import java.util.ArrayList;
+
+/**
+ * @author Manoj.Mohanan Nair
+ * @Date 9/9/19
+ */
+
+// Reversing a string using recursion approach
+public class ReverseString {
+    public static void main(String[] args){
+
+        String input = "ABC123";
+        String output = reverseString(input);
+        System.out.println("the output is: " + output);
+    }
+
+    public static String reverseString(String input){
+        String first = input.substring(0,1);
+        String second = input.substring(1);
+        if(second.length()<=1){
+            return second + first;
+        }
+        second = reverseString(second);
+        String output =second + first;
+        System.out.println("Inside the recursion: " + output);
+        return output;
+    }
+}
