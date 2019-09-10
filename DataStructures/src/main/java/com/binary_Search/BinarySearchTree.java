20afe431fba1ef8be9d0c6f5a20891894a1a2bf0
manoj <manojm_nair@outlook.com>
2019-09-10 01:43:38 -0700

    binary trees

--- DataStructures/src/main/java/com/binary_Search/BinarySearchTree.java
+++ DataStructures/src/main/java/com/binary_Search/BinarySearchTree.java
@@ -1,170 +1,215 @@
-
 package com.binary_Search;
+
+import java.util.NoSuchElementException;
+
 /**
-    This class implements a binary search com.tree whose
-    nodes hold objects that implement the Comparable
-    interface.
+ * @author Manoj.Mohanan Nair
+ * @Date 9/9/19
  */
- public class BinarySearchTree
- {
-    private Node root;
+public class BinarySearchTree {
+    public static void main(String[] args){
+        BinarySearchTree bn = new BinarySearchTree();
 
-     /**
-        Constructs an empty com.tree.
-      */
-     public BinarySearchTree()
-     {
-        root = null;
-     }
+        bn.insert(4);
+        bn.insert(2);
+        bn.insert(1);
+        bn.insert(8);
+        bn.insert(6);
+        bn.insert(5);
+        bn.insert(12);
+        bn.insert(9);
+        bn.insert(3);
+        System.out.println("Completed the insertion");
+        bn.display(bn.root);
+        boolean status = bn.find(6);
+        System.out.println("Completed the finding: " +status);
+        bn.delete(5);
+        System.out.println("Completed the deletion");
+        bn.display(bn.root);
+        System.out.println("Completed the display");
+    }
+    class Node{
+        Object data;
+        Node left;
+        Node right;
 
-     /**
-         Inserts a new node into the com.tree.
-        @param obj the object to insert
-     */
-     public void add(Comparable obj)
-     {
-        Node newNode = new Node();
-        newNode.data = obj;
-        newNode.left = null;
-        newNode.right = null;
-        if (root == null) root = newNode;
-        else root.addNode(newNode);
-     }
+        public Node(Object element){
+            this.data = element;
+            this.left = this.right = null;
+        }
+    }
+    public Node root;
 
-     /**
-        Tries to find an object in the com.tree.
-        @param obj  the object to find
-        @return true if the object is contained in the com.tree
-     */
-     public boolean find(Comparable obj)
-     {
-        Node current = root;
-        while (current != null)
-        {
-           int d = current.data.compareTo(obj);
-           if (d == 0) return true;
-           else if (d > 0) current = current.left;
-           else current = current.right;
+    // based on the value of n, compare with root.data
+    // if root.data>n, then go left else go right.
+    // go until we find the leaves ie null child i.e return false
+    public boolean find_recursion(Object n){
+
+        if(root== null){
+            throw new NoSuchElementException();
+        }
+        if((int)root.data == (int)n){
+            return true;
+        } else if((int)root.data > (int)n && (root.left != null)){
+            find_recursion(root.left.data);
+        } else if((int)root.data < (int)n &&  (root.right != null)){
+            find_recursion(root.right.data);
         }
         return false;
-     }
+    }
 
-     /**
-        Tries to remove an object from the com.tree. Does nothing
-        if the object is not contained in the com.tree.
-        @param obj the object to remove
-     */
-     public void remove(Comparable obj)
-     {
-        // Find node to be removed
-	    Node toBeRemoved = root;
-	    Node parent = null;
-	    boolean found = false;
-	    while (!found && toBeRemoved != null)
-	    {
-	        int d = toBeRemoved.data.compareTo(obj);
-	        if (d == 0) found = true;
-	        else
-	        {
-	          parent = toBeRemoved;
-	          if (d > 0) toBeRemoved = toBeRemoved.left;
-	          else toBeRemoved = toBeRemoved.right;
-	        }
-	    }
-
-	    if (!found) return;	
-	    
-	    // toBeRemoved contains  obj	
-	    // If one of the children is empty, use the other	
-	    if (toBeRemoved.left == null || toBeRemoved.right == null)
-	    {
-	       Node newChild;
-	       if (toBeRemoved.left == null)
-	          newChild = toBeRemoved.right;
-	       else
-	          newChild = toBeRemoved.left;
-	
-	       if (parent == null) //  Found in root
-	          root = newChild;
-	       else if (parent.left == toBeRemoved)
-	          parent.left = newChild;
-	       else
-	             parent.right = newChild;
-	      return;
-	    }
-	    
-	    //  Neither subtree is empty
-	    // Find smallest element of the right subtree
-	     Node smallestParent = toBeRemoved;
-	     Node smallest = toBeRemoved.right;
-	     while (smallest.left != null)
-	     {
-	         smallestParent = smallest;
-	         smallest = smallest.left;
-	     }
-	     
-	     // smallest  contains smallest child in right subtree
-	     //  Move contents, unlink child
-	     toBeRemoved.data = smallest.data;
-	     if (smallestParent == toBeRemoved)
-	         smallestParent.right = smallest.right;
-	     else
-	         smallestParent.left = smallest.right;
-     }
-     
-     /**
-        Prints the contents of the com.tree in sorted order.
-     */
-     public void print()
-     {
-           if (root != null)
-              root.printNodes();
-           System.out.println();
-     }
-
-     /**
-        A node of a com.tree stores a data item and references
-        to the child nodes to the left and to the right.
-     */
-     class Node
-     {
-        public Comparable data;
-        public Node left;
-        public Node right;
-
-        /**
-           Inserts a new node as a descendant of this node.
-           @param newNode     the node to insert
-        */
-        public void addNode(Node newNode)
-        {
-           int comp = newNode.data.compareTo(data);
-           if (comp < 0)
-           {
-              if (left == null) left = newNode;
-              else left.addNode(newNode);
-           }
-              if (comp > 0)
-           {
-              if (right == null) right = newNode;
-              else right.addNode(newNode);
-           }
+    public Node getSuccessor(Node deleleNode){
+        Node successsor =null;
+        Node successsorParent =null;
+        Node current = deleleNode.right;
+        while(current!=null){
+            successsorParent = successsor;
+            successsor = current;
+            current = current.left;
         }
+        //check if successor has the right child, it cannot have left child for sure
+        // if it does have the right child, add it to the left of successorParent.
+//		successsorParent
+        if(successsor!=deleleNode.right){
+            successsorParent.left = successsor.right;
+            successsor.right = deleleNode.right;
+        }
+        return successsor;
+    }
 
-        /**
-              Prints this node and all of its descendants
-              in sorted order.
-        */
-        public void printNodes()
-        {
-        	//traverse the left nodes
-              if (left != null)
-                 left.printNodes();
-              //print the root node data i.e root.key value
-              System.out.print(data + " ");
-              //traverse the right nodes
-              if (right != null)
-                 right.printNodes();
+    // based on the value of n, compare with root.data
+    // if root.data>n, then go left else go right.
+    // go until we find the leaves ie null child i.e return false
+    public boolean find(Object n){
+        Node current = root;
+
+        while(current!=null){
+            if((int)current.data == (int)n){
+                return true;
+            } else if((int)current.data > (int)n){
+                current = current.left;
+            } else if((int)current.data < (int)n){
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    // Use the same logic of find() to find the node where value is good.
+    // iterate till the node is null and then add this node there to the leaf
+    public void insert(Object n){
+        Node newNode = new Node(n);
+        if(root == null){
+            root = newNode;
+            return;
+        }
+        Node current = root;
+        while(current != null){
+            if((int)current.data >= (int)n){
+                if(current.left == null){
+                    current.left = newNode;
+                    return;
+                }else {
+                    current = current.left;
+                }
+
+            } else if((int)current.data < (int)n){
+                if(current.right == null){
+                    current.right = newNode;
+                    return;
+                }else {
+                    current = current.right;
+                }
             }
         }
     }
+
+    /** Node to be deleted is a leaf node
+     *      set parent.left = null or parent.right = null
+     *  Node to be deleted has only one child
+     *      Traverse to the node.
+     *      keep track of the parent node and the left or right child node
+     *      check for child node which is null
+     *      remove the entire subtree and add it to parent tree is on the delete node.
+     *  Node to be deleted has two child
+     *      Find the succesor node which will replace the deleted node
+     *      successor is the smallest node in the right subtree of the node to be deleted
+    */
+    public boolean delete(Object del){
+
+        Node parent = root;
+        Node current = root;
+        boolean isLeftChild = false;
+        while((int)current.data!=(int)del){
+            parent = current;
+            if((int)current.data>(int)del){
+                isLeftChild = true;
+                current = current.left;
+            }else{
+                isLeftChild = false;
+                current = current.right;
+            }
+            if(current ==null){
+                return false;
+            }
+        }
+        //if i am here that means we have found the node
+        //Case 1: if node to be deleted has no children
+        if(current.left==null && current.right==null){
+            if(current==root){
+                root = null;
+            }
+            if(isLeftChild ==true){
+                parent.left = null;
+            }else{
+                parent.right = null;
+            }
+        }
+        //Case 2 : if node to be deleted has only one child
+        else if(current.right==null){
+            if(current==root){
+                root = current.left;
+            }else if(isLeftChild){
+                parent.left = current.left;
+            }else{
+                parent.right = current.left;
+            }
+        }
+        else if(current.left==null){
+            if(current==root){
+                root = current.right;
+            }else if(isLeftChild){
+                parent.left = current.right;
+            }else{
+                parent.right = current.right;
+            }
+        }else if(current.left!=null && current.right!=null){
+
+            //now we have found the minimum element in the right sub tree
+            Node successor	 = getSuccessor(current);
+            if(current==root){
+                root = successor;
+            }else if(isLeftChild){
+                parent.left = successor;
+            }else{
+                parent.right = successor;
+            }
+            successor.left = current.left;
+        }
+        return true;
+    }
+    // Print the entire tree in the increasing order
+    // Have to use recursion, I see no other options.
+    public void display(Node root){
+        Node current = root;
+
+        System.out.println(root.data);
+        if(root.left!=null){
+            display(root.left);
+        }
+        if(root.right != null) {
+            display(root.right);
+        }
+        return;
+    }
+}
