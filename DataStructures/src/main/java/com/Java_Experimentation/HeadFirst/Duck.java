1e6c9c07708e7e15656ba809d5c1a22ea179ac6a
manojis <manojm_nair@outlook.com>
2023-06-07 00:10:33 -0700

    June-8 Tutort and LruCache Changes

---
+++ DataStructures/src/main/java/com/Java_Experimentation/HeadFirst/Duck.java
@@ -0,0 +1,13 @@
+package com.Java_Experimentation.HeadFirst;
+
+public class Duck {
+    protected void fly(){
+        System.out.println("This duck can fly");
+    }
+    protected void quack(){
+        System.out.println("This duck can quack'");
+    }
+    protected void swim(){
+        System.out.println("This duck can swim'");
+    }
+}
