b0c2d8777e9268a4a91559c6312e3b98e9ca1ddc
manojis <manojm_nair@outlook.com>
2024-07-27 15:49:38 -0700

    test

--- DataStructures/src/main/java/com/leetcode/leetcode75/ThreeSums.java
+++ DataStructures/src/main/java/com/leetcode/leetcode75/ThreeSums.java
@@ -1,4 +1,13 @@
 package com.leetcode.leetcode75;
 
+import java.util.List;
+
 public class ThreeSums {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
 }
