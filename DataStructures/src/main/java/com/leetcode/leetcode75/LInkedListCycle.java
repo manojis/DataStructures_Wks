873708b4055ddbbf0a4e69d5d6965fcac4ce13d6
manoj.mohanan_nair <manoj.mohanan_nair@daimler.com>
2024-09-22 23:20:18 -0700

    LinkedListCycle

---
+++ DataStructures/src/main/java/com/leetcode/leetcode75/LInkedListCycle.java
@@ -0,0 +1,34 @@
+package com.leetcode.leetcode75;
+
+public class LInkedListCycle {
+  public static void main(String [] args) {
+
+  }
+
+  public boolean hasCycle(ListNode head) {
+    // create two pointers for slow and fast pointer
+    // Assign two variables fast and slow
+    // loop through nodes until fast.next.next != null
+    // when fast.next.next = slow.next then return true
+        /*             0
+                   0       0
+          0 0 0 0             0
+                   0       0
+                       0    */
+    if (head == null) {
+      return false;
+    }
+    ListNode slow = head;
+    ListNode fast = head.next;
+
+    while(fast!= null && fast.next!= null) {
+      if (fast == slow) {
+        return true;
+      } else {
+        slow = slow.next;
+        fast = fast.next.next;
+      }
+    }
+    return false;
+  }
+}
