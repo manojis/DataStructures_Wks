b255b5bfc547089556f3b0bd9dded63d06587d4a
manoj.mohanan_nair <manoj.mohanan_nair@daimler.com>
2024-09-24 22:21:02 -0700

    test

---
+++ DataStructures/src/main/java/com/leetcode/leetcode75/RemoveAdjacentDuplicates_1047.java
@@ -0,0 +1,15 @@
+package com.leetcode.leetcode75;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class RemoveAdjacentDuplicates_1047 {
+  public static void main(String[] args) {
+    RemoveAdjacentDuplicates_1047 removeAdjacentDuplicates = new RemoveAdjacentDuplicates_1047();
+    System.out.println(removeAdjacentDuplicates.removeDuplicates("azxxzy"));
+  }
+  public String removeDuplicates(String s) {
+    
+  }
+}
